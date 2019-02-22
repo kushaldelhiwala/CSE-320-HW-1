756666069db81ac30c4aabbceebe65dcdd6332c7
Kushal Delhiwala <kdelhiwala@cs.stonybrook.edu>
2019-02-22 23:54:24 +0000

    File Reading stuff

--- main.c
+++ main.c
@@ -6,48 +6,60 @@
 int stringcomp(char *string1, char *string2) {
     while (*string1 && *string2 && *string1 == *string2){
     	 string1++;
-	 string2++; 
+	 string2++;
     }
     return (unsigned char)(*string1) - (unsigned char)(*string2);
 }
+
+int myStringLength(char *str){
+	int length = 0;
+	while (*str != 0){
+		str++;
+		length++;
+	}
+	return length;
+}
+
+typedef struct art_pieces{
+	struct art_pieces* next;
+	struct art_pieces* prev;
+	int art_id;
+	char *art_type;
+	char *art_name;
+	char *artist_name;
+	int price;
+} art_pieces;
 
 int main(int argc, char** argv) {
 	int b_flag = 0;
 	char *budget;
 	int int_budget;
-
 	int v_flag = 0;
-
 	int i_flag = 0;
 	char *id;
 	int int_id;
-
 	int t_flag = 0;
 	char *type;
-
 	int n_flag = 0;
 	char *artist_name;
-
 	int o_flag = 0;
 	char *store_file_name;
-	char *user_input;	//Ask Sergey if correct way
-
+	char *user_input;				// Ask Sergey if correct way
 	int file_name_token = 0;
 	char *char_filename;
-	
 	int i;
+	FILE *storefile;
+	FILE *inputfile;
+	art_pieces *collection;				// Main Database
 
-	FILE *cmdfile;
-	
 	if (argc < 3){
            printf("NO QUERY PROVIDED\n");
      	 }
 
      	else if (argc >= 3){
-    	  
     	  for (i = 0; i < argc; i++) {
-    	  	char *arg_i = *(argv + i);				
-				
+    	  	char *arg_i = *(argv + i);
+
 		if (stringcomp (arg_i, "./art_collector") == 0){}
 
 		else if (stringcomp (arg_i, "-b") == 0){
@@ -71,7 +83,7 @@ int main(int argc, char** argv) {
 			if (i_flag || t_flag || n_flag == 1){
 				printf("ERROR");
 			}
-					
+
 			v_flag = 1;
 		}
 
@@ -92,7 +104,7 @@ int main(int argc, char** argv) {
 					printf("ID ERROR\n");
 				}
 			}
-					
+
 		}
 
 		else if (stringcomp (arg_i, "-t") == 0){
@@ -104,7 +116,7 @@ int main(int argc, char** argv) {
 				i++;
 				type = *(argv + i);
 			}
-					
+
 		}
 
 		else if (stringcomp (arg_i, "-n") == 0){
@@ -117,21 +129,22 @@ int main(int argc, char** argv) {
 				i++;
 				artist_name = *(argv + i);
 			}
-					
 		}
 
 		else if (stringcomp (arg_i, "-o") == 0){
 			o_flag = 1; 
 			i++;
 			store_file_name = *(argv + i);
-			
+
 			//Ask Sergey if proper 
 
 			if (access(store_file_name, F_OK) != -1){
 				// File exists
 				printf("Should the file be overwritten?\n");
 				scanf("%s", user_input);
-				if (stringcomp(user_input, "yes") == 0 || stringcomp(user_input, "y") == 0){}
+				if (stringcomp(user_input, "yes") == 0 || stringcomp(user_input, "y") == 0){
+					storefile = fopen(store_file_name, "w");
+				}
 
 				else if (stringcomp(user_input, "no") == 0|| stringcomp(user_input, "n") == 0){
 					printf("FILE EXISTS\n");
@@ -140,11 +153,12 @@ int main(int argc, char** argv) {
 
 			}
 			else{
-				cmdfile = fopen(store_file_name, "w");
+				storefile = fopen(store_file_name, "w");
+				//When do we close?
 			}
 
 		}
-				
+
 		else {
 			if (file_name_token == 0){
 				char_filename = *(argv + i);
@@ -155,14 +169,130 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
-		//printf("%s\n", budget);
-		//printf("%s\n", char_filename);
-		//printf("%d\n", i);
-		//printf("%d\n", file_name_token);
-		//printf("%d\n", b_flag);
+
+}
+
+	// Check if input file exists or not
+
+	if (access(char_filename, F_OK)!= -1){
+		inputfile=fopen(char_filename, "rt");
+
+	char* art_type = (char*)malloc(25 * sizeof(char));
+		char* art_name = (char*)malloc(25 * sizeof(char));
+		char* artist_name = (char*)malloc(25 * sizeof(char));
+		char* command = (char*)malloc(25 * sizeof(char));
+
+		int art_id;
+		int price;
+		int ret = -1;
+
+	char* singleLine = (char*)malloc(150 * sizeof(char));
+
+	    while(!feof(inputfile)){
+    		fgets(singleLine, 150, inputfile);
+
+		if (*singleLine == 'B' || *singleLine == 'U'){
+    			ret = sscanf(singleLine, "%*s %d %s %s %s %d ", &art_id, art_type, art_name, artist_name, &price);
+
+    			if (ret != 5)
+    				ret = sscanf(singleLine, "%*s %d %s \"%[^\"]\" %s %d ", &art_id, art_type, art_name, artist_name, &price);
+
+			if (ret != 5)
+    				ret = sscanf(singleLine, "%*s %d %s \"%[^\"]\" \"%[^\"]\" %d ", &art_id, art_type, art_name, artist_name, &price);
+
+    			if (ret != 5)
+    				ret = sscanf(singleLine, "%*s %d %s %s \"%[^\"]\" %d ", &art_id, art_type, art_name, artist_name, &price);
+		}
+
+    			else if (*singleLine == 'S'){
+    				ret = sscanf(singleLine, "%d %d", &art_id, &price);
+			}
+
+			else{
+    				printf("ERROR: INVALID COMMAND\n");
+				free(singleLine);
+				free(art_type);
+				free(art_name);
+				free(artist_name);
+				fclose(inputfile);
+				return -1;
+    			}
+
+		if (art_id < 0){
+    			printf("ERROR: ART ID LESS THAN 0\n");
+			free(singleLine);
+			free(art_type);
+			free(art_name);
+			free(artist_name);
+			fclose(inputfile);
+			return -1;
+    		}
+
+    		if (myStringLength(art_type) > 10){
+    			printf("ERROR: ART TYPE GREATER THAN 10\n");
+			free(singleLine);
+			free(art_type);
+			free(art_name);
+			free(artist_name);
+			fclose(inputfile);
+			return -1;
+    		}
+			else{
+    				*art_type = toupper(*art_type);
+    			}
+
+		if(myStringLength(art_name) < 3 || myStringLength(art_name) > 50){
+    			printf("ERROR: ART NAME LENGTH ERROR\n");
+			free(singleLine);
+			free(art_type);
+			free(art_name);
+			free(artist_name);
+			fclose(inputfile);
+			return -1;
+    		}
+    			else{
+    				*art_name = toupper(*art_name);
+    			}
+
+		if(myStringLength(artist_name) < 3 || myStringLength(artist_name) > 50){
+    			printf("ERROR: ARTIST NAME LENGTH ERROR\n");
+			free(singleLine);
+			free(art_type);
+			free(art_name);
+			free(artist_name);
+			fclose(inputfile);
+			return -1;
+    		}
+    			else{
+    				*artist_name = toupper(*artist_name);
+    			}
+
+    		if(price < 0){
+    			printf("ERROR: INVALID PRICE\n");
+			free(singleLine);
+			free(art_type);
+			free(art_name);
+			free(artist_name);
+			fclose(inputfile);
+			return -1;
+    		}
+
 	}
-	
+	free(singleLine);
+	free(art_type);
+	free(art_name);
+	free(artist_name);
+	fclose(inputfile);
+	//printf("%d\n", ret);
+    	return 0;
+	}
+
+	else{
+		// File does not exist
+		printf("FILENAME DOES NOT EXIST\n");
+	}
 	return 0;
-    
+  	
+
 
 }
